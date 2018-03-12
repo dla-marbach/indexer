20e2638afa2f904adf08ced7b04ae077ce94c41f
Juergen Enge <juergen@info-age.net>
2018-03-12 15:52:00 +0100

    error correction

---
+++ cli/do_od.sh
@@ -0,0 +1,10 @@
+#php recurse.php hd
+php index.php libmagic hd
+php index.php gvfsinfo hd
+php index.php md5 hd
+php index.php imagemagick hd
+php index.php avconv hd
+php index.php antiword hd
+php index.php nsrl hd
+php index.php tika hd
+php solr.php hd
